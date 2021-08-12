bd9869052ec8ae32f66aeb71eef4ffa01e93acdc
KatielleRezende <88806340+KatielleRezende@users.noreply.github.com>
2021-08-12 20:36:24 -0300

    Incluidas assertivas para o body do teste incluirPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,7 +1,5 @@
-// 1-Pacote
-
+// 1 - Pacote
 package petstore;
-
 
 // 2 - Bibliotecas
 
@@ -12,35 +10,42 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
-// 3 - Classes
+// 3 - Classe
 public class Pet {
-    //3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet";// endereço da entidade Pet
+    // 3.1 - Atributos
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
 
-
-    // 3.2 - Metodos e Funções
-public String lerJson(String caminhoJason) throws IOException {
-
-        return new String(Files.readAllBytes(Paths.get(caminhoJason)));
+    // 3.2 - Métodos e Funções
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
-    // Incluir - Create - Post
-    @Test // Identifica o método ou função como um teste para o TestNG
-    public void incluirPet() throws IOException {
-    String JsonBody = lerJson("db/pet1.Json");
 
-    // Sintaxe Gherkin
+    // Incluir - Create - Post
+    @Test  // Identifica o método ou função como um teste para o TestNG
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        // Sintaxe Gherkin
         // Dado - Quando - Então
         // Given - When - Then
+
         given() // Dado
-                .contentType("application/Json") // comum em API REST - antigas era "text/xml"
+                .contentType("application/json") // comum em API REST - antigas era "text/xml"
                 .log().all()
-                .body(JsonBody)
-        .when()  // Quando
+                .body(jsonBody)
+                .when()  // Quando
                 .post(uri)
-        .then() // Então
+                .then()  // Então
                 .log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("name", is("Thor"))
+                .body("status", is("available"))
+        ;
+
 
     }
+
+
 }
